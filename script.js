0cb8be28b4ad14ba6b66616e272f3ffd46c7ccf0
Krshnkmr <krishna_anbalagan.1993cs@yahoo.com>
2020-10-30 15:47:14 +0530

    3 files modified

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('hello!')
+$(window).scroll(function () {
+    let position = $(this).scrollTop();
+    // console.log(position);
+    if (position >= 170) {
+        $('.navbar-css').addClass('custom-navbar');
+    }
+    else {
+        $('.navbar-css').removeClass('custom-navbar');
+    }
+});
+$(document).ready(function () {
+    // alert("width of carousel " + $(".carousel-item").width());
+    // let position1= $(this).scrollRight();
+    // console.log("the position of width is  " + position1);
+    console.log("width of carousel " + $(".carousel-item").width());
+});
